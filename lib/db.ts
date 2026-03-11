6bf31f040367d2e5dd3c8c7fc8ab409d1c5620b7
Fortune <fortunexbt@proton.me>
2026-03-11 14:10:25 +0100

    fix: explicitly disable ssl: false for postgres connection

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,8 @@
 import postgres from 'postgres'
 
-// Railway provides the complete DATABASE_URL in the environment
-// We need to disable SSL rejection to allow the connection over the proxy.
 const sql = process.env.DATABASE_URL
   ? postgres(process.env.DATABASE_URL, {
-      ssl: { rejectUnauthorized: false },
+      ssl: false,
     })
   : null
 
