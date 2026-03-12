8d69cd43683a3f3a2312af0e3221d7378ddc56d5
Fortune <fortunexbt@proton.me>
2026-03-12 18:40:12 +0100

    test(web): isolate Jest auth helper test from Playwright

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   testEnvironment: 'node',
-  roots: ['<rootDir>/tests/app'],
+  roots: ['<rootDir>/tests/unit'],
   transform: {
     '^.+\\.(t|j)sx?$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
   },
